72e4633404db7f6339b82bbb3490bebd6a414db5
jhapriya909 <thepriyajha8840@gmail.com>
2026-02-21 23:58:09 +0530

    d:\C++_Journey\Lets_Learn_cpp\divide.cpp

---
+++ divide.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a = 5;
+    int b = 2;
+    cout<< (a/b) <<endl;
+
+    cout<< (5/2) <<endl; // answer = 2
+
+    // short divide using type casting 
+
+    cout<< (8 / (double)3) <<endl;
+}
