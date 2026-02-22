8b67b44db68f83ac3ab093a8f4e7b7f1abefe70f
jhapriya909 <thepriyajha8840@gmail.com>
2026-02-22 21:25:18 +0530

    d:\C++_Journey\Lets_Learn_cpp\ifelseevenorodd.cpp

---
+++ ifelseevenorodd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << " Enter any number";
+    cin >> n;
+
+    if (n % 2 == 0)
+    {
+        cout << " Even number \n";
+    }
+    else
+    {
+        cout << " Odd number \n";
+    }
+    return 0;
+}
