2104d8e18a5403515da79089787218f4d226c800
jhapriya909 <thepriyajha8840@gmail.com>
2026-02-22 21:22:32 +0530

    d:\C++_Journey\Lets_Learn_cpp\character.cpp

---
+++ character.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout << " Enter character";
+    cin >> ch;
+    if (ch >= 'a' && ch <= 'z')
+    {
+        cout << " Lower case \n";
+    }
+
+    else 
+    {
+        cout << " Upper case \n";
+    }
+    return 0;
+    
+}
