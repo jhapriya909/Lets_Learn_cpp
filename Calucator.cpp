8508a1cb8dca714adf2a0d0b0d9ab0669e683166
jhapriya909 <thepriyajha8840@gmail.com>
2026-02-23 18:28:03 +0530

    d:\C++_Journey\Lets_Learn_cpp\Calucator.cpp

---
+++ Calucator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << " Enter fist  number:" << endl;
+    cin >> a;
+
+    cout << " Enter Second number:" << endl;
+    cin >> b;
+
+    cout << " sum  = " << (a + b) << endl;
+    cout << " difference = " << (a - b) << endl;
+    cout << " product = " << (a * b) << endl;
+    cout << " division = " << (a / b) << endl;
+    cout << " modulo = " << (a % b) << endl;
+    return 0;
+}
