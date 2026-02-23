50df685c578098074978777aae6f2fbeeecbd3cb
jhapriya909 <thepriyajha8840@gmail.com>
2026-02-23 18:28:42 +0530

    d:\C++_Journey\Lets_Learn_cpp\Increment.cpp

---
+++ Increment.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    int b = ++a; //  update ,kaam
+
+    cout << " b  = " << b << endl; // 11
+    cout << " a  = " << a << endl; // 11
+    return 0;
+}
