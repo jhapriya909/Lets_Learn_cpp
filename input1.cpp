e5f4aca19cbbfec33210c26ab8627f4472688baa
jhapriya909 <thepriyajha8840@gmail.com>
2026-02-23 18:29:34 +0530

    d:\C++_Journey\Lets_Learn_cpp\input1.cpp

---
+++ input1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double price;
+    cout << " Enter a price" << endl;
+    cin >> price;
+
+    cout << " your price is  = " << price << endl;
+}
