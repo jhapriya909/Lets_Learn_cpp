04133a25340570a93c8a44260c511c08a36d4f06
jhapriya909 <thepriyajha8840@gmail.com>
2026-02-24 20:25:58 +0530

    d:\C++_Journey\Lets_Learn_cpp\Relational.cpp

---
+++ Relational.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    cout << (3 < 5) << endl;  // true ---> 1
+
+    cout << (3 <= 5) << endl; // true ---> 1
+
+    cout << (3 > 5) << endl;  // false ---> 0
+
+    cout << (3 >= 5) << endl; // false ---> 0
+
+    cout << (3 == 5) << endl; // false ---> 0
+
+    cout << (3 != 5) << endl; // true ---> 1
+
+    cout << (3 <= 3) << endl; // true ---> 1
+    
+    cout << (3 != 3) << endl; // false ---> 0
+    return 0;
+}
