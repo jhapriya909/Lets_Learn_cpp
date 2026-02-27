5b57c237128eca29401d4c7eeef17f1955c63187
jhapriya909 <thepriyajha8840@gmail.com>
2026-02-27 20:32:10 +0530

    d:\C++_Journey\Lets_Learn_cpp\size.cpp

---
+++ size.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age = 25;
+    cout << age << endl;
+    cout << sizeof(age) << endl;
+
+    char grade = 'A';
+    cout << grade << endl;
+    cout << sizeof(grade) << endl;
+
+    float PI = 3.14f;
+    cout << PI << endl;
+    cout << sizeof(PI) << endl;
+
+    bool isSafe = true;
+    cout << isSafe << endl;
+    cout << sizeof(isSafe) << endl;
+
+    double price = 100.91;
+     cout << price << endl;
+    cout << sizeof(price) << endl;
+
+    return 0;
+}
