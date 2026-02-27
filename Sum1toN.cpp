74890157eff9b5c9f78b9a81643219980f020e85
jhapriya909 <thepriyajha8840@gmail.com>
2026-02-27 20:33:40 +0530

    d:\C++_Journey\Lets_Learn_cpp\Sum1toN.cpp

---
+++ Sum1toN.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 5, sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    cout << " sum = " <<  sum << " " << endl;
+    return 0;
+}
