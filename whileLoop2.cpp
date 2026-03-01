d3f061d930176a5ac2bfd97493066814fb947e48
jhapriya909 <thepriyajha8840@gmail.com>
2026-03-01 20:41:24 +0530

    d:\C++_Journey\Lets_Learn_cpp\whileLoop2.cpp

---
+++ whileLoop2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int  n = 20;
+    int count = 1;
+
+    while (count <= n)
+    {
+        cout << count << " ";
+
+        count++;
+    }
+   cout << endl;
+    return 0;
+}
