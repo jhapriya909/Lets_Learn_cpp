2fa670097a19d61887704146c334a4e130079bcd
jhapriya909 <thepriyajha8840@gmail.com>
2026-03-20 18:35:16 +0530

    e:\C++_Journey\Lets_Learn_cpp\Patterns\Hollowdiamond.cpp

---
+++ Patterns/Hollowdiamond.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 4;
+
+    // top
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        // spaces
+        cout << "* ";
+
+        if (i != 0)
+        {
+            for (int j = 0; j < 2 * i - 1; j++)
+            {
+                cout << " ";
+            }
+
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    // bottom 
+     for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <i+1; j++)
+        {
+            cout << " ";
+        }
+        // spaces
+        cout << "* ";
+
+        if (i != n-1)
+        {
+            for (int j = 0; j < 2 *(n-i)-5; j++)
+            {
+                cout << " ";
+            }
+
+            cout << "* ";
+        }
+        cout << endl;
+
+}
+}
